319919db27ff13cfd5bcfc4dcf89773438dfe98d
rati <ratibregvadze@gmail.com>
2023-01-22 19:47:14 +0400

    change key

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import {Configuration, OpenAIApi} from 'openai';
 import {useState} from "react";
 
-const OPEN_AI_KEY= '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'
+const OPEN_AI_KEY= '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'
 
 function App() {
   const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("https://oaidalleapiprodscus.blob.core.windows.net/private/org-4F9CB6ZMw8anEk2lLRTWytOH/user-aRUi9IYO1E0pU1nbCBgARV5G/img-Pw7U3FLokZmqr1NaXIRLBkrH.png?st=2023-01-22T14%3A13%3A14Z&se=2023-01-22T16%3A13%3A14Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-01-22T12%3A36%3A58Z&ske=2023-01-23T12%3A36%3A58Z&sks=b&skv=2021-08-06&sig=GDJqFSuGfwNQgM6ejlZsZE4GIisHtrYZVEMfhPoyHdg%3D");
+  const [result, setResult] = useState("");
   const configuration  = new Configuration({
     apiKey: OPEN_AI_KEY,
   });
